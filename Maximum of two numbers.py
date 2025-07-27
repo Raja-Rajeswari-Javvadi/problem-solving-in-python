70f99e2591b697938d7e338e61c19a1ceca6d2a4
Raja Rajeswari Javvadi <146972116+Raja-Rajeswari-Javvadi@users.noreply.github.com>
2025-07-27 12:45:46 +0530

    Add files via upload

---
+++ Maximum of two numbers.py
@@ -0,0 +1,8 @@
+#static
+a=20
+b=30
+print(max(a,b))
+#dynamic
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b : "))
+print(max(a,b))
---
+++ even or odd.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the value of a : "))
+if a%2==0:
+    print("Even")
+else:
+    print("Odd")
