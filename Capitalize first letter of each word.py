4c10930d3409746fcd3eb4454ce453354fbeb2c5
Raja Rajeswari Javvadi <146972116+Raja-Rajeswari-Javvadi@users.noreply.github.com>
2025-08-10 15:50:32 +0530

    Add files via upload

---
+++ Capitalize first letter of each word.py
@@ -0,0 +1,4 @@
+def capitalize_word(sentence):
+    return sentence.title()
+sentence=input("Enter a sentence : ")
+print("After Capitalizing first letter of each word : ",capitalize_word(sentence))
